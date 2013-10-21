9ca3e56d61b3facc0a326a3c07c92c662dd9abfe
rstandke <rainer.standke@etecture.de>
2013-10-21 09:04:23 +0200

    Added test for south and north

--- src/test/java/de/rstandke/RoverTest.java
+++ src/test/java/de/rstandke/RoverTest.java
@@ -58,6 +58,33 @@ public class RoverTest {
         assertThat(rover.getPositionOnGrid().getX()).isEqualTo(4);
     }
 
+    @Test
+    public void go_DirectionIsSouthtCommandsAreBFF_3() throws Exception {
+        // arrange
+
+        // act
+        Rover rover = new Rover(new Position(2, 2), Direction.SOUTH);
+        rover.go(Command.BACKWARD, Command.FORWARD, Command.FORWARD);
+
+        // assert
+        assertThat(rover).isNotNull();
+        assertThat(rover.getPositionOnGrid().getY()).isEqualTo(3);
+    }
+
+    @Test
+    public void go_DirectionIsNorthCommandsAreBFF_3() throws Exception {
+        // arrange
+
+        // act
+        Rover rover = new Rover(new Position(2, 2), Direction.NORTH);
+        rover.go(Command.BACKWARD, Command.FORWARD, Command.FORWARD);
+
+        // assert
+        assertThat(rover).isNotNull();
+        assertThat(rover.getPositionOnGrid().getY()).isEqualTo(1);
+    }
+
+
 
 
 
